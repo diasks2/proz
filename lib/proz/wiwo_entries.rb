3b89476b281bbbfe2201f8b8e6569e939c33f00f
Kevin Dias <diasks2@gmail.com>
2016-07-06 13:20:52 -0400

    Add token auth

--- lib/proz/wiwo_entries.rb
+++ lib/proz/wiwo_entries.rb
@@ -3,10 +3,14 @@ require 'httparty'
 module Proz
   class WiwoEntries
     include HTTParty
-     base_uri "https://api.proz.com/v2"
+    base_uri "https://api.proz.com/v2"
+    attr_reader :token
+    def initialize(token:)
+      @token = token
+    end
 
     def entries
-      self.class.get("/workstatus/search")
+      self.class.get("/workstatus/search", headers: { 'Authorization' => "Bearer #{token}" })
     end
   end
 end
