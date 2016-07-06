9887a2afb773bbc2340f85fbf07e180ae90c791e
Kevin Dias <diasks2@gmail.com>
2016-07-06 13:04:54 -0400

    Add wiwo options

--- lib/proz/wiwo_entry.rb
+++ lib/proz/wiwo_entry.rb
@@ -4,18 +4,17 @@ module Proz
   class WiwoEntry
     include HTTParty
     base_uri "https://api.proz.com/v2"
-    attr_reader :token, :message
-    def initialize(token:, message:)
+    attr_reader :token, :message, :options
+    def initialize(token:, message:, **options)
       @token = token
-      @message = message
+      @message = { message: message }
+      @options = options
     end
 
     def post
+      body = message.merge!(options)
       self.class.post("/workstatus",
-        body: { message: message,
-                source_language: "eng",
-                target_language: "spa",
-                wordcount: "234" },
+        body: body,
         headers: { 'Authorization' => "Bearer #{token}" } )
     end
   end
