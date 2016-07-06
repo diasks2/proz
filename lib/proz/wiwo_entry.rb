975b0d743788fb9e3b8db7ed01ca7cb28fac8e4d
Kevin Dias <diasks2@gmail.com>
2016-07-06 12:53:45 -0400

    Update wiwo post

--- lib/proz/wiwo_entry.rb
+++ lib/proz/wiwo_entry.rb
@@ -4,17 +4,18 @@ module Proz
   class WiwoEntry
     include HTTParty
     base_uri "https://api.proz.com/v2"
-    attr_reader :token
-    def initialize(token:)
+    attr_reader :token, :message
+    def initialize(token:, message:)
       @token = token
+      @message = message
     end
 
     def post
-      puts self.class.post("/workstatus",
-        body: { message: 'This is a test from TMT',
+      self.class.post("/workstatus",
+        body: { message: message,
                 source_language: "eng",
                 target_language: "spa",
-                wordcount: "234" }.to_json,
+                wordcount: "234" },
         headers: { 'Authorization' => "Bearer #{token}" } )
     end
   end
