b4f386724a73b36316158a3789ba74a899e992a9
Kevin Dias <diasks2@gmail.com>
2016-07-13 07:42:11 +0900

    Update GET Wiwo end point

--- lib/proz/wiwo_entries.rb
+++ lib/proz/wiwo_entries.rb
@@ -4,17 +4,25 @@ module Proz
   class WiwoEntries
     include HTTParty
     base_uri "https://api.proz.com/v2"
-    attr_reader :token
-    def initialize(token:)
-      @token = token
+    attr_reader :api_key
+    def initialize(api_key:)
+      @api_key = api_key
     end
 
     def entries
-      self.class.get("/wiwo", headers: { 'Authorization' => "Bearer #{token}" })
+      self.class.get("/wiwo", headers: { 'X-Proz-API-Key' => api_key })
+    end
+
+    def wiwo_entries
+      self.class.get("/wiwo", headers: { 'X-Proz-API-Key' => api_key }).entries["wiwos"]
+    end
+
+    def user_entries
+      self.class.get("/wiwo", headers: { 'X-Proz-API-Key' => api_key }).entries["users"]
     end
 
     def user_entries(user_uuid)
-      self.class.get("/wiwo?user_uuid=" + user_uuid, headers: { 'Authorization' => "Bearer #{token}" })
+      self.class.get("/wiwo?user_uuid=" + user_uuid, headers: { 'X-Proz-API-Key' => api_key })
     end
   end
 end
